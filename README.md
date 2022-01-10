26cf4f6530f989911149c35c29bda27c67b3da5c
Inga Miadowicz <Inga.Miadowicz@gmail.com>
2022-01-10 17:24:02 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Overview repository AutomatedTestingCalculator
+
+## Goal
+Show examples on how code can be tested. 
+
+## Structure
+The repository is structured in two areas:
+1) main: main code with Calculator and Client class
+2) test: test code with for Calculator
+
+Each package contains subpackages with pattern names which contain the related code. Each subpackage can be started independently using the main method in Client or any Engine class. 
---
+++ src/main/java/com/dhbw/Calculator1.java
@@ -0,0 +1,109 @@
+package com.dhbw;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class Calculator1 {
+
+  private File output;
+  DatabaseHandler databaseHandler;
+
+  /**
+   * Normal constructor with file
+   * @param output
+   */
+  public Calculator1(File output) {
+    this.output = output;
+  }
+
+  /**
+   * Constructor for testing that allows mocks for dependent objects
+   * @param output
+   */
+  public Calculator1(File output, DatabaseHandler databaseHandler) {
+    this.output = output;
+    this.databaseHandler = databaseHandler;
+  }
+
+  /**
+   * Add two numbers
+   * @param a
+   * @param b
+   * @return
+   */
+  public BigInteger add(BigInteger a, BigInteger b) {
+    BigInteger result = BigInteger.valueOf(0);
+    try {
+      result = a.add(b);
+    } catch (ArithmeticException ex) {
+      throw new ArithmeticException("Can't add values " + a + " and " + b);
+    } catch (NullPointerException ex) {
+      throw new NullPointerException("Can't add with null values");
+    }
+    // new feature 1 write to file
+    writeToFile(a, b, result, "+");
+
+    // new feature 2 write to database
+    writeToDatabase(a, b, result, "+");
+    return result;
+  }
+
+  public void writeToFile(BigInteger a, BigInteger b, BigInteger result, String operation) {
+    try {
+      // create new, write and close file reader
+      BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(this.output));
+      bufferedWriter.write(a + " " + operation + " " + b + " = " + result);
+      bufferedWriter.close();
+    } catch (IOException ex) {
+      System.out.println("Can't write calculation history to file");
+    }
+
+  }
+
+  public boolean writeToDatabase(BigInteger a, BigInteger b, BigInteger result, String operation) {
+    try {
+      // write to database in case of production otherwise mock
+      if (this.databaseHandler == null) this.databaseHandler = new DatabaseHandler();
+      databaseHandler.writeToDatabase(a + " " + operation + " " + b + " = " + result);
+      return true;
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+      return false;
+    }
+  }
+
+
+  /**
+   * Substract two numbers
+   * @param a
+   * @param b
+   * @return
+   */
+  public int substract(int a, int b) {
+    return a-b;
+  }
+
+  /**
+   * Divide two numbers
+   * @param a
+   * @param b
+   * @return
+   */
+  public int divide(int a, int b) {
+    return a/b;
+  }
+
+  /**
+   * Multiply two numbers
+   * @param a
+   * @param b
+   * @return
+   */
+  public int multiply(int a, int b) {
+    return a*b;
+  }
+
+}
